dcfc293d639ae98995e4de6c9b455af35c608a47
kunal karmawat <kunalkarmavat@gmail.com>
2021-10-08 22:33:42 +0530

    first commit

---
+++ index.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int a,b,c;
+    cout << "Enter the first number : ";
+    cin >> a;
+    cout << "Enter the Second number: ";
+    cin >> b;
+    c= a+b;
+    cout << "Sum of a and b : "<< c << endl;
+}
